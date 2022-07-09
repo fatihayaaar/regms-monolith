a3cd30d19010c942821dd445a82f8deb353e4e69
Fayar <fatihayaaar@gmail.com>
2022-07-09 17:01:50 +0300

    Add files via upload

---
+++ java/membership-system/src/main/java/com/fayardev/membershipsystem/MembershipSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fayardev.membershipsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershipSystemApplication.class, args);
+    }
+
+}
---
+++ java/membership-system/src/test/java/com/fayardev/membershipsystem/MembershipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fayardev.membershipsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
